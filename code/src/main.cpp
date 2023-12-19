1fefcbccf1764a3b6586a081dd993a7c21c661a1
acm <a-mango@users.noreply.github.com>
2023-12-19 17:59:47 +0100

    rfr: set client and chair count to original values

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -13,8 +13,8 @@
 #include "application.h"
 #include "utils/graphicsaloninterface.h"
 
-constexpr unsigned int NB_CLIENTS = 30;
-constexpr unsigned int NB_SIEGES = 20;
+constexpr unsigned int NB_CLIENTS = 2;
+constexpr unsigned int NB_SIEGES = 3;
 
 int main(int argc, char *argv[])
 {
